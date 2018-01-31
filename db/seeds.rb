322215a38ce41b45cc95651a9d0426b923680409
Andrew Potter <hello@drewpotter.com>
2018-01-31 15:37:24 +0000

    Added questions to database seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,3 +16,119 @@ User.where(email: "test@test.com").first_or_create do |u|
   puts "Admin created! \nU: test@test.com\nP: test1234"
 end
 
+questions = [
+  {
+    text: "Humans are separate from nature, not part of it",
+  },
+  {
+    text: "Win-win outcomes for people and nature are rarely possible",
+  },
+  {
+    text: "Conservation will only succeed if it provides benefits for people",
+  },
+  {
+    text: "Conserving nature for nature's sake should be a goal of conservation",
+  },
+  {
+    text: "Conservation must benefit poor people because to do so is an ethical imperative",
+  },
+  {
+    text: "To achieve conservation goals, the environmental impact of the world's rich must be reduced",
+  },
+  {
+    text: "Conservation actions should primarily be informed by evidence from biological science",
+  },
+  {
+    text: "It is acceptable for people to be displaced to make space for protected areas",
+  },
+  {
+    text: "Pristine nature, untouched by human influences, does not exist",
+  },
+  {
+    text: "Strict protected areas are required to achieve most conservation goals",
+  },
+  {
+    text: "Nature often recovers from even severe perturbations",
+  },
+  {
+    text: "Conservation goals should be based on science",
+  },
+  {
+    text: "Protecting nature for its own sake does not work",
+  },
+  {
+    text: "There is no significant conservation value in highly modified landscapes",
+  },
+  {
+    text: "Conservation will only be a durable success if it has broad public support",
+  },
+  {
+    text: "Conservation should work with, not against, capitalism",
+  },
+  {
+    text: "Working with corporations is not just pragmatic; they can be a positive force for conservation",
+  },
+  {
+    text: "To achieve conservation goals, human population growth must be reduced",
+  },
+  {
+    text: "Human affection for nature grows in line with income",
+  },
+  {
+    text: "Advancing the wellbeing of all people should be a goal of conservation",
+  },
+  {
+    text: "Conservation should seek to reduce the emotional separation of people from nature",
+  },
+  {
+    text: "Conservation goals should be based on ethical values",
+  },
+  {
+    text: "Maintaining ecosystem processes should be a goal of conservation",
+  },
+  {
+    text: "Economic arguments for conservation are risky because they can lead to unintended negative conservation outcomes",
+  },
+  {
+    text: "Having multiple rationales for conservation weakens the conservation movement",
+  },
+  {
+    text: "Conservation messages that emphasise the value of nature for nature's own sake are more effective than those that promote the benefits of nature to humans",
+  },
+  {
+    text: "There is a risk that economic rationales for conservation will displace other motivations for conservation",
+  },
+  {
+    text: "Conservation communications are more effective when they use negative 'doom and gloom' messages rather than positive messages",
+  },
+  {
+    text: "Giving a voice to those affected by conservation actions improves conservation outcomes",
+  },
+  {
+    text: "To achieve its goals, conservation should seek to reform global trade",
+  },
+  {
+    text: "Non-native species offer little conservation value",
+  },
+  {
+    text: "Human impact on nature grows in line with incomes",
+  },
+  {
+    text: "Maintaining biological diversity should be a goal of conservation",
+  },
+  {
+    text: "Conservation will only be a durable success if it has the support of corporations",
+  },
+  {
+    text: "Conservation should seek to do no harm to poor people",
+  },
+  {
+    text: "Giving a voice to those affected by conservation action is an ethical imperative",
+  },
+  {
+    text: "The best way for conservation to contribute to human wellbeing is by promoting economic growth",
+  },
+  {
+    text: "When communities manage their own resources, their efforts are more effective than top-down approaches",
+  }
+]
