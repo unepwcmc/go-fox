65847ac91b87e599291687cc3499b5fbc973d682
Andrew Potter <hello@drewpotter.com>
2018-02-02 16:56:36 +0000

    Added support for new response creation and ip address

--- app/controllers/responses_controller.rb
+++ app/controllers/responses_controller.rb
@@ -6,9 +6,34 @@ class ResponsesController < ApplicationController
     @response = Response.new
   end
 
+  def create
+    @response = Response.new(response_params)
+    @response.survey = Survey.find_by_uuid(params[:survey_uuid])
+    @response.ip_address = request.remote_ip
+
+    respond_to do |format|
+      if @response.save
+        format.html { redirect_to root_path, notice: 'Response was successfully created.' }
+        format.json { render :show, status: :created, location: @response }
+      else
+        format.html { render :new }
+        format.json { render json: @response.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def show
+  end
+
   private
     def require_survey_published
       # return if @survey.published?
       # redirect_to root_path, notice: "You cannot view an unpublished survey"
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def response_params
+      #params.require(:response).permit(:name, :published)
+      return nil
     end
 end
