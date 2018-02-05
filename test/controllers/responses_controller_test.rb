ed19b7750933a6d7a1fa4a8980b848520c686dfb
Andrew Potter <hello@drewpotter.com>
2018-02-05 15:16:39 +0000

    Initial work towards testing

--- test/controllers/responses_controller_test.rb
+++ test/controllers/responses_controller_test.rb
@@ -1,7 +1,47 @@
 require 'test_helper'
 
 class ResponsesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  include Devise::Test::IntegrationHelpers
+
+  setup do
+    @user  = create(:user)
+    @admin = create(:admin)
+  end
+
+  test "response can only be destroyed by a logged in user" do
+    @survey   = create(:survey, user: @user)
+    @response = create(:response, survey: @survey)
+
+    delete survey_response_url(@survey, @response)
+    assert_redirected_to new_user_session_path
+  end
+
+  test "response can only be deleted by owner of the survey" do
+    @survey   = create(:survey, user: @admin)
+    @response = create(:response, survey: @survey)
+
+    sign_in @user
+    delete survey_response_url(@survey, @response)
+    assert_redirected_to survey_path(@survey)
+  end
+
+  test "a user cannot delete another user's reponses" do
+    assert false
+  end
+
+  test "an admin can delete any response" do
+    assert false
+  end
+
+  test "show can not be accessed wihtout being logged in" do
+    assert false
+  end
+
+  test "new can be accessed by anyone" do
+    assert false
+  end
+
+  test "create can be accessed by anyone" do
+    assert false
+  end
 end
