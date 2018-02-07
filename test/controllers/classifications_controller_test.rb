63d86e8ee2d8b8f2bf4bd2bcab057e4d29050e80
Andrew Potter <hello@drewpotter.com>
2018-02-07 08:54:05 +0000

    Fixed broken tests

--- test/controllers/classifications_controller_test.rb
+++ test/controllers/classifications_controller_test.rb
@@ -1,21 +1,28 @@
 require 'test_helper'
 
 class ClassificationsControllerTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
+
   setup do
-    @classification = classifications(:one)
+    @classification = create(:classification)
+    @user  = create(:user)
+    @admin = create(:admin)
   end
 
   test "should get index" do
+    sign_in @admin
     get classifications_url
     assert_response :success
   end
 
   test "should get new" do
+    sign_in @admin
     get new_classification_url
     assert_response :success
   end
 
   test "should create classification" do
+    sign_in @admin
     assert_difference('Classification.count') do
       post classifications_url, params: { classification: { description: @classification.description, name: @classification.name, results_description: @classification.results_description } }
     end
@@ -24,21 +31,25 @@ class ClassificationsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should show classification" do
+    sign_in @admin
     get classification_url(@classification)
     assert_response :success
   end
 
   test "should get edit" do
+    sign_in @admin
     get edit_classification_url(@classification)
     assert_response :success
   end
 
   test "should update classification" do
+    sign_in @admin
     patch classification_url(@classification), params: { classification: { description: @classification.description, name: @classification.name, results_description: @classification.results_description } }
     assert_redirected_to classification_url(@classification)
   end
 
   test "should destroy classification" do
+    sign_in @admin
     assert_difference('Classification.count', -1) do
       delete classification_url(@classification)
     end
