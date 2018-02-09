ac339a39ab14e663d9127f1804b28680df989ee2
Andrew Potter <hello@drewpotter.com>
2018-02-09 15:48:39 +0000

    Display the language of a response on the show page

--- config/application.rb
+++ config/application.rb
@@ -15,7 +15,7 @@ module GoFox
 
     I18n.load_path += Dir[Rails.root.join('lib', 'locale', '*.{rb,yml}')]
 
-    I18n.available_locales = [:en, :es, :fr, :de]
+    I18n.available_locales = [:en, :es, :fr]
 
     I18n.default_locale = :en
 
