15e94501dc030d2360d46aba351f79ce3cafa24b
Andrew Potter <hello@drewpotter.com>
2018-02-15 16:37:57 +0000

    Added hidden fields to text question

--- app/javascript/components/TextQuestion.js
+++ app/javascript/components/TextQuestion.js
@@ -5,13 +5,14 @@ export default class TextQuestion extends React.Component {
   // Takes in a text question and renders the form
 
   render () {
-    const fieldName = this.props.model + "[" + this.props.attribute + "][" + this.props.index + "][" + this.props.field + "]"
-
+    const fieldName = this.props.model + "[" + this.props.attribute + "][" + this.props.index + "]"
     return(
       <div>
         <h3>{this.props.question.text}</h3>
         <p>
-          <input name={fieldName} type="text"/>
+          <input name={fieldName + "[raw]"} type="text"/>
+          <input name={fieldName + "[answerable_type]"} type="hidden" value="DemographicQuestion"/>
+          <input name={fieldName + "[answerable_id]"} type="hidden" value={this.props.question.id}/>
         </p>
       </div>
     );
@@ -22,4 +23,3 @@ export default class TextQuestion extends React.Component {
   // text:     PropTypes.string.isRequired,
   // answers:  PropTypes.array.isRequired
 // };
-
