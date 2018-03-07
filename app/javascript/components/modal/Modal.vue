bf449f25bdedce9518c0d777f22503f16113c79c
Stacy Talbot <stacymtalbot@gmail.com>
2018-03-07 14:20:10 +0000

    Remove code not needed

--- app/javascript/components/modal/Modal.vue
+++ app/javascript/components/modal/Modal.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="modal" :class="{ 'modal--active' : isActive }" :style="{ 'top' : topPosition }">
+  <div class="modal" :class="{ 'modal--active' : isActive }">
 
     <div class="modal__content">
       <button class="button--plain modal__close" @click="closeModal">
@@ -17,31 +17,14 @@
   export default {
     name: 'modal',
 
-    data () {
-      return {
-        config: {
-          smallBreakpoint: 720 // must match scss file
-        },
-        modalContent: {},
-        top: 0,
-        hero: true
-      }
-    },
-
     created () {
       eventHub.$on('openModal', this.openModal)
-
-      eventHub.$on('getScrollY', this.getScrollY)
     },
 
     computed: {
       isActive () {
         return this.$store.state.modal.isActive
-      },
-
-      topPosition () {
-        return this.top + 'px'
-      },
+      }
     },
 
     methods: {
@@ -49,29 +32,8 @@
         this.$store.commit('modal/updateModalStatus')
       },
 
-      openModal ( hero = true ) {
-        this.hero = hero
-
-        // get the y position and open the modal and modal wrapper
-        this.getScrollY()
-
-        // update the modal with the latest event content
-        if(!this.hero){ this.modalContent = this.$store.state.modal.modalContent }
-
-        this.toggleModal()
-      },
-
       closeModal () {
         this.toggleModal()
-      },
-
-      getScrollY () {
-        // add top padding if being viewed on medium or large breakpoints
-        this.top = window.innerWidth <= this.config.smallBreakpoint ? window.pageYOffset : window.pageYOffset + 40
-      },
-
-      hasContent (property) {
-        return !!property
       }
     }
   }
