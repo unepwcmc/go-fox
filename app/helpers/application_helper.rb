55e2a353272ad214bc81e9ebbd6f6507fa07ba89
Stacy Talbot <stacymtalbot@gmail.com>
2018-03-12 09:54:22 +0000

    Add 'my account' partial for the modal and create helper methods for devise

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def resource_name
+    current_user
+  end
+
+  def resource
+    @resource ||= User.new
+  end
+
+  def devise_mapping
+    @devise_mapping ||= Devise.mappings[:user]
+  end
 end
