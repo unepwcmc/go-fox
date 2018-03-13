f71683fe9613006f254aec32eb02aecbf7b693cc
Stacy Talbot <stacymtalbot@gmail.com>
2018-03-13 13:52:29 +0000

    Add content to the public facing home page

---
+++ app/helpers/static_pages_helper.rb
@@ -0,0 +1,18 @@
+module StaticPagesHelper
+  def homepageContent
+    [
+      {
+        title: 'What is the purpose of the project',
+        description: 'The purpose of this project is to explore the views of conservationists on a range of issues, as a way of informing debates on the future of conservation. Recent debates about the future of conservation have been dominated by a few high-profile individuals, whose views seem to fit fairly neatly into polarised positions.',
+        url: '/',
+        icon: ''
+      },
+      {
+        title: 'See how you sit in the movement with easily digestible graphical data visualisations',
+        description: 'The purpose of this project is to explore the views of conservationists on a range of issues, as a way of informing debates on the future of conservation. Recent debates about the future of conservation have been dominated by a few high-profile individuals, whose views seem to fit fairly neatly into polarised positions.',
+        url: '/',
+        icon: ''
+      }
+    ]
+  end
+end
