b9aee7138acba30c8e4012bd2552249fdd09b9ad
Stacy Talbot <stacymtalbot@gmail.com>
2018-03-27 14:06:45 +0100

    Move about projects content from the helper file to the locales

--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -1,47 +1,4 @@
 module StaticPagesHelper
-  def aboutProjectContent
-    [
-      {
-        link: 'How does it work',
-        title: 'How does it work lorem ipsum dolor sit amet, consectetur adipiscing elit',
-        content: '<h3>Sub-heading 1</h3><p>The purpose of th is project is to explore the views of conservationists on a range of issues, as a way of informing debates on the future of conservation. Recent debates about the future of conservation have been dominated by a few high-profile individuals, whose views seem to fit fairly neatly into polarised positions. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim</p><h4>Sub-heading 2</h4><p>The purpose of th is project is to explore the views of conservationists on a range of issues, as a way of informing debates on the future of conservation. Recent debates about the future of conservation have been dominated by a few high-profile individuals, whose views seem to fit fairly neatly into polarised positions. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim.</p>'
-      },
-      {
-        link: 'About us',
-        title: 'About us',
-        content: '<p>The purpose of th is project is to explore the views of conservationists on a range of issues, as a way of informing debates on the future of conservation. Recent debates about the future of conservation have been dominated by a few high-profile individuals, whose views seem to fit fairly neatly into polarised positions. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim</p><p>The purpose of th is project is to explore the views of conservationists on a range of issues, as a way of informing debates on the future of conservation. Recent debates about the future of conservation have been dominated by a few high-profile individuals, whose views seem to fit fairly neatly into polarised positions. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim.</p>'
-      },
-      {
-        link: 'Contact',
-        title: 'Contact us',
-        content: '<p>The purpose of th is project is to explore the views of conservationists on a range of issues, as a way of informing debates on the future of conservation. Recent debates about the future of conservation have been dominated by a few high-profile individuals, whose views seem to fit fairly neatly into polarised positions. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim</p><p>The purpose of th is project is to explore the views of conservationists on a range of issues, as a way of informing debates on the future of conservation. Recent debates about the future of conservation have been dominated by a few high-profile individuals, whose views seem to fit fairly neatly into polarised positions. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim.</p>'
-      },
-      {
-        link: 'FAQs',
-        title: 'FAQs',
-        content: 'Several people have been in touch with questions about various aspects of the Future of Conservation project. Rather than write to everyone individually, we have prepared the following answers to some frequently asked questions. If you still have a question that is not answered below, please do get in touch!',
-        accordion: [
-          {
-            title: 'The wording of some of the questions is problematic, making it difficult to know how to answer?',
-            content: '<p>The survey went through a lengthy period of testing, but like all social surveys, we accept that not all the questions are perfect and that some people will dislike some of them. It is important to note that as the survey was intended to discover how conservationists responded to the points made in the New Conservation debate in the literature, many of the statements are direct quotes from published articles in that debate.</p>'
-          },
-          {
-            title: 'The wording of some of the questions is problematic, making it difficult to know how to answer?',
-            content: '<p>The survey went through a lengthy period of testing, but like all social surveys, we accept that not all the questions are perfect and that some people will dislike some of them. It is important to note that as the survey was intended to discover how conservationists responded to the points made in the New Conservation debate in the literature, many of the statements are direct quotes from published articles in that debate.</p>'
-          },
-          {
-            title: 'The wording of some of the questions is problematic?',
-            content: '<p>It is important to note that as the survey was intended to discover how conservationists responded to the points made in the New Conservation debate in the literature, many of the statements are direct quotes from published articles in that debate.</p>'
-          },
-          {
-            title: 'The wording of some of the questions is problematic?',
-            content: '<p>It is important to note that as the survey was intended to discover how conservationists responded to the points made in the New Conservation debate in the literature, many of the statements are direct quotes from published articles in that debate.</p>'
-          }
-        ]
-      }
-    ]
-  end
-
   def aboutDebateContent
     [
       {
