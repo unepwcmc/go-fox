f1f930dc8d5e6c4fa68cf51e81a0873bb304b8b2
Stacy Talbot <stacymtalbot@gmail.com>
2018-05-08 16:03:46 +0100

    Add tabs vye component and re-build the login page to math the new design

---
+++ app/javascript/components/tabs/Tab.vue
@@ -0,0 +1,48 @@
+<template>
+  <div v-show="isActive" class="tab">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'tab',
+
+    props: {
+      title: { 
+        required: true,
+        type: String 
+      },
+      active: { type: Boolean }
+    },
+
+    data () {
+      return {
+        isActive: false
+      }
+    },
+
+    created () {
+      if (this.active) { this.isActive = this.active }
+
+      this.id = 'tab' + this.removeWhiteSpaces(this.title)
+    }, 
+
+    methods: {
+      removeWhiteSpaces (string) {
+        const regex = new RegExp(/\s/g)
+
+        return string.replace(regex, g => { return '' })
+      }
+    }
+  }
+</script>
+
+<style lang="scss">
+  .tab {
+
+    &__trigger {
+      cursor: pointer;
+    }
+  }  
+</style>
