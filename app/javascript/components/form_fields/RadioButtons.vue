799195e9b31b4bc6517a5834dfc4ade8e1f7b438
Stacy Talbot <stacymtalbot@gmail.com>
2018-05-09 12:36:17 +0100

    Add click event to checkbox input and label wrapper to trigger the checkbox

--- app/javascript/components/form_fields/RadioButtons.vue
+++ app/javascript/components/form_fields/RadioButtons.vue
@@ -15,7 +15,7 @@
     
     <div v-else class="flex flex-wrap">
       <p v-for="option in options" class="radio-button flex flex-1-half" @click="toggleRadio(option.text)">
-        <input required type="radio" v-model="input" :value="option.text" :name="name" :id="id(option.text)" class="radio-button__input">
+        <input required type="radio" v-model="input" :value="option.text" :name="name" :id="id(option.text)">
         <label :for="id(option.text)" :class="labelClass(option.text)" class="radio-button__label">{{ option.text }}</label>
       </p>
     </div>
