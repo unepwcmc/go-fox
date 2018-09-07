ab11b2f9ff760c73ee01862347f6840c301ba11b
Andrew Potter <hello@drewpotter.com>
2018-09-07 09:36:51 +0100

    Added admin root path as authenticated user default root

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   devise_for :users
 
   authenticated :user do
-    root 'surveys#index', as: :authenticated_root
+    root 'admin/dashboard#index', as: :authenticated_root
   end
 
   root to: "static_pages#index"
