d766fdf4a39b5f41d2ca17b14e34ea73606d1406
Andrew Potter <hello@drewpotter.com>
2018-10-15 11:03:54 +0100

    Add settings jsonb field to surveys

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180920144651) do
+ActiveRecord::Schema.define(version: 20181015095235) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -159,6 +159,7 @@ ActiveRecord::Schema.define(version: 20180920144651) do
     t.text "description"
     t.boolean "locked", default: false
     t.boolean "master", default: false
+    t.jsonb "settings", default: {}
     t.index ["user_id"], name: "index_surveys_on_user_id"
     t.index ["uuid"], name: "index_surveys_on_uuid", unique: true
   end
