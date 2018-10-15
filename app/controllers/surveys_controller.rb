bbef05fec4efb7d4e5ff8c570ad0f2f738a7517d
Andrew Potter <hello@drewpotter.com>
2018-10-15 13:20:18 +0100

    Work in progress on the survey settings jsonb

--- app/controllers/surveys_controller.rb
+++ app/controllers/surveys_controller.rb
@@ -93,6 +93,7 @@ class SurveysController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
     def survey_params
       params.require(:survey).permit(:name, :description, :published, :locked,
+                                     settings: [:survey_previous_id, :participant_org_name, :course_url, :session_date, :hope_to_achieve],
                                      translations_attributes: [:id, :name, :description, :locale, :_destroy],
                                      customised_questions_attributes: [:id, :text, :demographic_question_id, :locale, :_destroy,
                                        options_attributes: [:id, :optionable_id, :optionable_type, :text, :locale, :_destroy]
