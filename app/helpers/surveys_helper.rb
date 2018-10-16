1b8157dfd27976352b4d418409e4895f306aa2ce
Andrew Potter <hello@drewpotter.com>
2018-10-16 09:41:35 +0100

    Added more radio button fields for other questions for new survey creation form

--- app/helpers/surveys_helper.rb
+++ app/helpers/surveys_helper.rb
@@ -1,8 +1,14 @@
 module SurveysHelper
-  def radio_button_data(type)
+  def surveys_data(type)
     case type
     when "default_language"
       default_language
+    when "participant_level_of_study"
+      participant_level_of_study
+    when "participant_specialism"
+      participant_specialism
+    when "follow_up_session"
+      follow_up_session
     else []
     end
   end
@@ -14,6 +20,34 @@ module SurveysHelper
       en: "English",
       es: "Spanish",
       fr: "French"
+    }
+  end
+
+  def participant_level_of_study
+    {
+      undergraduate: "Undergraduate degree",
+      postgraduate: "Postgraduate degree",
+      vocational: "Vocational qualification",
+      training: "Training course",
+      other: "Other (please specify):"
+    }
+  end
+
+  def participant_specialism
+    {
+      interdisciplinary: "Interdisciplinary",
+      biological: "Biological sciences",
+      non_biological: "Non-biological natural sciences",
+      social: "Social sciences",
+      humanities: "Humanities",
+      na: "Not applicable"
+    }
+  end
+
+  def follow_up_session
+    {
+      yes: "Yes",
+      no: "No"
     }
   end
 end
