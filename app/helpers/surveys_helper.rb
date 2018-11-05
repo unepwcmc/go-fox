763d0f87898bfcc36cb5655fe9f82beab625b64a
Andrew Potter <hello@drewpotter.com>
2018-11-05 16:50:38 +0000

    More progress on the organisational user new survey form

--- app/helpers/surveys_helper.rb
+++ app/helpers/surveys_helper.rb
@@ -13,6 +13,12 @@ module SurveysHelper
       anonymity
     when "location_of_organisation"
       location_of_organisation
+    when "type_of_organisation"
+      type_of_organisation
+    when "organisation_funding_sources"
+      organisation_funding_sources
+    when "wider_network"
+      wider_network
     else []
     end
   end
@@ -64,6 +70,41 @@ module SurveysHelper
 
   def location_of_organisation
     COUNTRIES
+  end
+
+  def type_of_organisation
+    {
+      ngo: "Non Governmental Organisation (NGO)",
+      government: "Governmental Organisation",
+      intergovernmental: "Intergovernmental Organisation",
+      private_sector: "Private Sector",
+      inter_organisation_collaboration: "Inter-Organisation group / collaboration",
+      university: "University",
+      training: "Training College",
+      other: "Other (please specify)"
+    }
+  end
+
+  def organisation_funding_sources
+    {
+      individual_subscriptions: "Individual member subscriptions",
+      small_donations: "Small donations from members of the public",
+      large_donations: "Large donations from wealthy individuals and foundations",
+      research_grants: "Research grants",
+      government_funding: "Government funding",
+      commercial_income: "Commercial income",
+      corporate_partnerships: "Corporate partnerships/donations",
+      intergovernmental_funding: "Intergovernmental funding",
+      wider_network: "A wider network or partnership to which the group/organisation belongs",
+      other: "Other (please specify)"
+    }
+  end
+
+  def wider_network
+    {
+      yes: "Yes",
+      no: "No"
+    }
   end
 
 end
