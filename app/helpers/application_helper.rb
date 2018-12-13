d7eb810c9f9817f54c77f244221ac1796709f4f7
Will Kocur <will.kocur@unep-wcmc.org>
2018-12-13 12:08:14 +0000

    dummy sharing urls - tbc

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,25 +11,46 @@ module ApplicationHelper
     @devise_mapping ||= Devise.mappings[:user]
   end
 
+  def current_page
+    request.fullpath
+  end
+
+  def encoded_current_url
+    url_encode(request.base_url + current_page)
+  end
+
   def current_class?(test_path)
-    request.fullpath == test_path ? ' nav--active' : ''
+    current_page == test_path ? ' nav--active' : ''
   end
 
   def is_current_page?(test_path)
-    request.fullpath == test_path
+    current_page == test_path
   end
 
-  def social_urls
-    {
-      facebook: 'https://www.facebook.com',
-      twitter: 'https://www.twitter.com'
-    }
+  def create_social_facebook_link
+    title = url_encode('Share your results on Facebook')
+    url = encoded_current_url
+    href = 'https://facebook.com/sharer/sharer.php?u=' + url
+
+    link_to '', href, title: title, class: 'icon--social icon--facebook', target: '_blank'
   end
 
-  def create_social_link(site)
-    title = url_encode("Follow Go Fox on #{site}")
-    href = social_urls[site.downcase.to_sym]
+  def create_social_twitter_link
+    title = url_encode('Share your results on Twitter')
+    text = url_encode('Some tweet text')
+    url = encoded_current_url
+    href = 'https://twitter.com/intent/tweet/?text=' + text + '&url=' + url
+    
+    link_to '', href, title: title, class: 'icon--social icon--twitter', target: '_blank'
+  end
 
-    link_to '', href, title: title, class: "icon--social icon--#{site.downcase}", target: '_blank'
+  def create_social_email_link
+    title = url_encode('Share your results via Email')
+    url = encoded_current_url
+    subject = url_encode('a subject')
+    body = 'site_description' + url_encode("\n\n") + url
+    href = 'mailto:?subject=' + subject + '&body=' + body
+
+    link_to '', href, title: title, class: 'icon--social icon--mail', target: '_self'
   end
 end
