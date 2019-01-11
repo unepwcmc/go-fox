b541d8673977d7789ca7cc3531efa3c0cfbfa881
Will Kocur <will.kocur@unep-wcmc.org>
2019-01-11 13:20:20 +0000

    prevent submitting on enter click on sign up form

--- app/javascript/helpers/application-helpers.js
+++ app/javascript/helpers/application-helpers.js
@@ -1,20 +1,35 @@
-import { isNull } from "util";
-
 const removeSpecialChars = str => str.replace(/[^a-zA-Z0-9 -]/g, '')
 
 export const nameToId = str => removeSpecialChars(str).replace(/ /g, '-').toLowerCase()
 
+const initializeCustomFormEventHandling = () => {
+  $(document).ready(() => {
+    $(window).keydown(event => {
+      const ENTER_KEY_CODE = 13
+      const isFormWithPreventedEnter = event => $(event.target.form).data('enterPreventDefault')
+
+      if(event.keyCode == ENTER_KEY_CODE && isFormWithPreventedEnter(event)) {
+        event.preventDefault()
+        return false
+      }
+    })
+  })
+}
+
+export const initializeEventHandling = () => {
+  initializeCustomFormEventHandling()
+}
 
 export const addStickyStyling = (elementId, stickyOffset=0, initialOffsetTop=0) => {
   const stickyElement = $('#' + elementId)
 
-  // if(isNaN(initialOffsetTop)) {
+    // if(isNaN(initialOffsetTop)) {
   //   window.setTimeout(
   //     () => { initialOffsetTop = stickyElement.offset().top },
   //     100
   //   )
   // }
-
+  
   $(document).scroll(() => {
     const scrollDistance = $(document).scrollTop()
 
