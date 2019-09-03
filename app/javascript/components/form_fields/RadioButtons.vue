81e86cdb3c3ffe0bc1485a92cceb0cf10d732684
Will Kocur <will.kocur@unep-wcmc.org>
2019-09-03 09:46:29 +0100

    use name for value of radio buttons

--- app/javascript/components/form_fields/RadioButtons.vue
+++ app/javascript/components/form_fields/RadioButtons.vue
@@ -8,15 +8,15 @@
       <div v-for="option in options" class="radio-button--scale">
 
         <input required type="radio" v-model="input" :value="option.name" :name="name" :id="id(option.name)" class="radio-button__input">
-        <label :for="id(option.name)" :class="labelClass(option.text)" class="radio-button__label flex flex-column flex-h-center">{{ option.text }}</label>
+        <label :for="id(option.name)" :class="labelClass(option.name)" class="radio-button__label flex flex-column flex-h-center">{{ option.text }}</label>
         <i class="radio-button__tick"></i>
       </div>
     </div>
     
     <div v-else class="flex-m-t-stack flex-wrap">
       <p v-for="option in options" class="radio-button flex flex-1-half flex-v-center" @click="toggleRadio(option.text)">
-        <input required type="radio" v-model="input" :value="option.text" :name="name" :id="id(option.text)">
-        <label :for="id(option.text)" :class="labelClass(option.text)" class="radio-button__label">{{ option.text }}</label>
+        <input required type="radio" v-model="input" :value="option.name" :name="name" :id="id(option.name)">
+        <label :for="id(option.name)" :class="labelClass(option.name)" class="radio-button__label">{{ option.text }}</label>
       </p>
     </div>
   </div>
