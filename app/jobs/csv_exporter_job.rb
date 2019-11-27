9c6e2643c2af3b724663219e2ae06b252ea89c5f
Andrew Potter <hello@drewpotter.com>
2019-11-27 15:38:23 +0000

    Remove user_id as it is not needed

--- app/jobs/csv_exporter_job.rb
+++ app/jobs/csv_exporter_job.rb
@@ -3,7 +3,7 @@ require_relative '../../lib/modules/csv_exporter'
 class CsvExporterJob < ApplicationJob
   queue_as :default
 
-  def perform(to_email, survey=nil, from_date=nil, to_date=nil, user_id=nil)
+  def perform(to_email, survey=nil, from_date=nil, to_date=nil)
     filepath = CsvExporter.export(survey, from_date, to_date, User.find_by(email: to_email).id)
     NotificationMailer.csv_exported(filepath.to_s, to_email).deliver_now
   end
