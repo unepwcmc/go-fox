05bedbbb043fe6ce2e31fa8a77d4455cb0d5c469
Andrew Potter <hello@drewpotter.com>
2019-12-05 15:44:34 +0000

    Add :pt as an available locale

--- config/initializers/i18n.rb
+++ config/initializers/i18n.rb
@@ -1,3 +1,3 @@
 I18n.load_path += Dir[Rails.root.join('lib', 'locale', '*.{rb,yml}')]
-I18n.available_locales = [:en, :es, :fr]
+I18n.available_locales = [:en, :es, :fr, :pt]
 I18n.default_locale = :en
