cbb278b0061eea7eae4f4e206c5da89642e63f8e
Will Kocur <will.kocur@unep-wcmc.org>
2021-03-19 17:32:17 +0000

    handle geographical_scope as string not array of strings

--- app/helpers/surveys_helper.rb
+++ app/helpers/surveys_helper.rb
@@ -4,6 +4,16 @@ module SurveysHelper
     master ? I18n.t('survey_warning.master_description') : I18n.t('survey_warning.description')
   end
 
+  def geographical_scopes(survey)
+    geographical_scope = survey.settings["geographical_scope"]
+
+    if geographical_scope.is_a? String
+      [surveys_data("geographical_scope")[geographical_scope]]
+    else
+      geographical_scope.map{ |k| surveys_data("geographical_scope")[k] }.compact!
+    end
+  end
+
   def surveys_data(type)
     return [] if type.nil?
     @data = YAML.load(File.open("#{Rails.root}/lib/data/content/surveys-form.yml", 'r'))
